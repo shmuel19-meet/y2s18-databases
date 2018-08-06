aef661ecf6b58c61ab232b8eabd1c3336b36281e
Shmuel19-meet <shmuel19-meet@meet.mit.edu>
2018-08-06 14:08:12 +0300

    k

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -3,13 +3,26 @@ from knowledge_model import Base, Knowledge
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///knowledge.db')
+Base = declarative_base()
+engine = create_engine('sqlite:///data.db')
 Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+
+def add(self, k_id, topic, name, rating):
+	"""
+	Add a student to the database, given
+	their name, year, and whether they have
+	finished the lab.
+	"""
+	student_object = Student(
+	name=name,
+	year=year,
+	finished_lab=finished_lab)
+	session.add(student_object)
+	session.commit()
+
 
 def query_all_articles():
 	pass
@@ -25,3 +38,7 @@ def delete_all_articles():
 
 def edit_article_rating():
 	pass
+
+a = Knowledge()
+a.add(1, "weather", "Rainbow", 9)
+print(__repr__(a))
--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -2,16 +2,40 @@ from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
+#from model import Base, Student
 
 Base = declarative_base()
 
 class Knowledge(Base):
 	# Create a table with 4 columns
+	def __init__(seld, Base):
+		__tablename__ = 'knowledge'
+		k_id = Column(Integer, primary_key=True)
+		name = Column(String)
+		topic = Column(String)
+		rating = Column(Integer)
+
+	def is_rainbow(self):
+		return self.topic == "wethear" and name == "Rainbow" and self.rating == 9 and self.k_id == 1
+
+	def is_bad(self):
+		return self.rating <= 7
+
+	
+#add_student("Mayuri", 2, True)
+
+	def __repr__(self):
+		
+		if is_rainbow():
+			return "If you want to learn about weather, you should look at the Wikipedia article called rainbow.\nWe gave this article a rating of 9 out of 10!"
+		
+		elif is_bad():
+			return "Unfortunately, this article does not have a better rating. Maybe, this is an article that should be\nreplaced soon!."
+
+		return(("id: {}\narticle name: {}\n""article topic: {}\n""rating: {}\n").format(self.k_id, self.name, self.topic, self.rating))
 	# The first column will be the primary key
 	# The second column should be a string representing
 	# the name of the Wiki article that you're referencing
 	# The third column will be a string representing the 
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
-
-	pass
